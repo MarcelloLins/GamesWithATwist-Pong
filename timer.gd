67ecb40a65fe3bef2c9743c4561b2cb14d8eb671
Marcello Lins <marcello.grechi@gmail.com>
2025-01-01 23:29:45 -0800

    Added Game timer at the bottom of the screen

---
+++ timer.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var timer :Timer = $"./Timer"
+@onready var label :Label = $"./Label"
+
+var timeLeft: int
+func _ready() -> void:
+	timeLeft = 60 # seconds
+	label.text = str(timeLeft)
+	timer.connect("timeout", update_label)
+
+func update_label():
+	timeLeft -= 1
+	label.text = str(timeLeft)
+	# restart timer
+	timer.start()
